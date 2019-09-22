65eeb9ffff3e385e7234eb678151dd6423bb6c69
tuffing <logan.orr@tuffing.co.nz>
2019-09-22 22:14:17 +1200

    project restructure

--- minify-for-release.sh
+++ minify-for-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Merging and minifying js"
-uglifyjs ynab_request.js ynab_config_manager.js ynab_categories_manager.js ynab_budgets_manager.js ynab_auth.js init.js -o minified/source.min.js --compress
+uglifyjs js/ynab_request.js js/ynab_config_manager.js js/ynab_categories_manager.js js/ynab_budgets_manager.js js/ynab_auth.js js/init.js -o minified/source.min.js --compress
 
 echo "Minifying css"
-uglifycss style.css --output minified/style.min.css 
+uglifycss css/style.css --output minified/style.min.css 
